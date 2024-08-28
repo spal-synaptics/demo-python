c3f69cdb56df9a3c7f758d7111698849d03ca9ce
spal-synaptics <sauryadeep.pal@synaptics.com>
2024-08-28 10:25:13 -0700

    Added descriptions for demo parameters

--- examples/demo_camera.py
+++ examples/demo_camera.py
@@ -7,36 +7,48 @@ from utils.model_info import get_model_input_dims
 from utils.user_input import get_inp_src_info
 
 
+# Your camera's input width.
 INPUT_WIDTH = 640
+
+# Your camera's input height.
 INPUT_HEIGHT = 480
+
+# Your camera's device ID, typically has the format "/dev/videoX". "AUTO" means the script will attempt to find your camera.
+# Note that "AUTO" might find the wrong camera if you have multiple cameras connected to the baord.
+# Execute `v4l2-ctl --list-devices` to get a list of connected camera devices and their device IDs.
 CAMERA = "AUTO"
+
+# The path to the inference model to use. Must be a vaild SyNAP model with a ".synap" file extension.
 MODEL = "/usr/share/synap/models/object_detection/coco/model/yolov8s-640x384/model.synap"
-INFERENCE_SKIP = 0
-INFERENCE_DELAY = 0
+
+# How many frames to skip between sucessive inferences.
+# Increasing this number may result in better performance but can look worse visually.
+INFERENCE_SKIP = 1
+
+# Whether to launch the demo in fullscreen.
 FULLSCREEN = True
 
 
-# ===========================
-# RUNNER CODE: DO NOT MODIFY
-# ===========================
+# ============================================================================== #
+# RUNNER CODE: DO NOT MODIFY                                                     #
+# ============================================================================== #
 
 def main():
-    camera, _, _ = get_inp_src_info(2, INPUT_WIDTH, INPUT_HEIGHT, CAMERA, None)
-    if not camera:
+    inp_src_info = get_inp_src_info(2, INPUT_WIDTH, INPUT_HEIGHT, CAMERA, None)
+    if not inp_src_info:
         sys.exit(1)
-    model_inp_w, model_inp_h = get_model_input_dims(MODEL)
-    if not model_inp_w or not model_inp_h:
+    model_inp_dims = get_model_input_dims(MODEL)
+    if not model_inp_dims:
         sys.exit(1)
     gst_params: dict[str, Any] = {
         "inp_type": 2,
         "inp_w": INPUT_WIDTH,
         "inp_h": INPUT_HEIGHT,
-        "inp_src": camera,
+        "inp_src": inp_src_info[0],
         "inf_model": MODEL,
-        "inf_w": model_inp_w,
-        "inf_h": model_inp_h,
+        "inf_w": model_inp_dims[0],
+        "inf_h": model_inp_dims[1],
         "inf_skip": INFERENCE_SKIP,
-        "inf_delay": INFERENCE_DELAY,
         "fullscreen": FULLSCREEN,
     }
 
@@ -53,3 +65,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# ============================================================================== #
