7cb1c47e6a31ad3cdb49c287d683152417427164
spal-synaptics <sauryadeep.pal@synaptics.com>
2024-08-28 11:53:16 -0700

    Added support for building mulitple .pyz at once

--- pyz_builder.py
+++ pyz_builder.py
@@ -14,29 +14,40 @@ if __name__ == "__main__":
     cwd: Path = Path(getcwd())
     examples: list[str] = [f.name for f in Path(cwd / "examples").glob("*.py")]
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument(
-        "-t", "--target",
+    t_group = parser.add_mutually_exclusive_group(required=True)
+    t_group.add_argument(
+        "--all",
+        action="store_true",
+        default=False,
+        help="Build all demos"
+    )
+    t_group.add_argument(
+        "-t", "--targets",
         type=str,
-        default="main.py",
-        choices=["main.py", *examples],
-        help="Which script should be executed (default: %(default)s)"
+        nargs="+",
+        metavar="DEMO",
+        default=["demo.py"],
+        choices=["demo.py", *examples],
+        help="Which demo(s) to build ([default] %(choices)s)"
     )
     args = parser.parse_args()
 
     if not (exec_dir := Path(cwd / "exec")).exists():
         exec_dir.mkdir(exist_ok=True)
 
-    with TemporaryDirectory() as td:
-        td = Path(td) / "demo-python"
-        for f in Path(".").rglob("*"):
-            if str(f.resolve()) == __file__:
-                continue
-            if f.suffix == ".py":
-                dst = td / f.parent
-                dst.mkdir(parents=True, exist_ok=True)
-                if f.name == args.target:
-                    copy2(f, td / "__main__.py")
-                else:
-                    copy2(f, dst)
-        exec_name = "demo_python" if args.target == "main.py" else Path(args.target).stem
-        zipapp.create_archive(td, f"exec/{exec_name}.pyz")
+    targets: list[str] = ["demo.py", *examples] if args.all else args.targets
+    for target in targets:
+        with TemporaryDirectory() as td:
+            td = Path(td) / "demo-python"
+            for f in Path(".").rglob("*"):
+                if str(f.resolve()) == __file__:
+                    continue
+                if f.suffix == ".py":
+                    dst = td / f.parent
+                    dst.mkdir(parents=True, exist_ok=True)
+                    if f.name == target:
+                        copy2(f, td / "__main__.py")
+                    else:
+                        copy2(f, dst)
+            zipapp.create_archive(td, f"exec/{Path(target).stem}.pyz")
+            print(f"Built .pyz for {target}")
